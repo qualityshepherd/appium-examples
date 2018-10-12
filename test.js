9d11bd2d7e4071909774bfb7bbcc45dc30fccd14
brine <brian@qualityshepherd.com>
2018-10-12 10:09:29 -0500

    es6 yes

--- test.js
+++ test.js
@@ -1,20 +1,20 @@
-var wd = require('wd'),
-testingbotKey = process.env.TESTINGBOT_KEY,
-testingbotSecret = process.env.TESTINGBOT_SECRET
+const wd = require('wd');
+const TESTINGBOT_KEY = process.env.TESTINGBOT_KEY;
+const TESTINGBOT_SECRET = process.env.TESTINGBOT_SECRET;
 
-desiredCaps = {
+const desiredCaps = {
     'app': 'http://appium.s3.amazonaws.com/ContactManager.apk',
     'version': '7.1',
     'platform': 'android',
     'deviceName': 'Pixel 2',
     'platformName': 'android',
     'name' : 'My First Mobile Test',
-    'client_key': testingbotKey,
-    'client_secret': testingbotSecret
+    'client_key': TESTINGBOT_KEY,
+    'client_secret': TESTINGBOT_SECRET
 }
 
-driver = wd.remote("https://" + testingbotKey + ":" + testingbotSecret + "@" + "hub.testingbot.com/wd/hub")
-driver.init(desiredCaps, function() {
+const driver = wd.remote("https://" + TESTINGBOT_KEY + ":" + TESTINGBOT_SECRET + "@" + "hub.testingbot.com/wd/hub")
+driver.init(desiredCaps, () => {
   console.log('App Started')
   driver.quit()
-})
+});
