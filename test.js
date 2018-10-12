c05fe66ed4907ba3ea44ea7a2827c7898130e48d
brine <brian@qualityshepherd.com>
2018-10-12 14:58:15 -0500

    more shenanigans

--- test.js
+++ test.js
@@ -1,20 +1,24 @@
 const wd = require('wd');
+// get testingbot credentials from env vars (you need to set these)
 const TESTINGBOT_KEY = process.env.TESTINGBOT_KEY;
 const TESTINGBOT_SECRET = process.env.TESTINGBOT_SECRET;
 
-const desiredCaps = {
+let desiredCapabilities = {
     'app': 'http://appium.s3.amazonaws.com/ContactManager.apk',
     'version': '7.1',
     'platform': 'android',
     'deviceName': 'Pixel 2',
     'platformName': 'android',
-    'name' : 'My First Mobile Test',
+    'browserName': 'chrome',
+    'name' : 'Appium Test',
     'client_key': TESTINGBOT_KEY,
     'client_secret': TESTINGBOT_SECRET
-}
+};
 
-const driver = wd.remote("https://" + TESTINGBOT_KEY + ":" + TESTINGBOT_SECRET + "@" + "hub.testingbot.com/wd/hub")
-driver.init(desiredCaps, () => {
-  console.log('App Started')
-  driver.quit()
+let driver = wd.remote("https://" + TESTINGBOT_KEY + ":" + TESTINGBOT_SECRET + "@" + "hub.testingbot.com/wd/hub");
+
+driver.init(desiredCapabilities, async () => {
+  console.log('HELLO WORLD');
+  driver.get('https://qualityshepherd.com');
+  driver.quit();
 });
